95bbc53cede24d3116153c8ce752a9e117b05001
heppu <henri.t.koski@gmail.com>
2026-07-15 11:38:41 +0300

    kanshi conf

--- kanshi/run.sh
+++ kanshi/run.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
-set +x
-
-# Wait until XDG_RUNTIME_DIR is set
-while [ -z "$XDG_RUNTIME_DIR" ] || [ -z "$WAYLAND_DISPLAY" ]; do
-  sleep 1
-done
-
-kanshi -c ~/.config/kanshi/$(hostname)
+kanshictl reload 2>/dev/null && exit 0
+exec kanshi -c ~/.config/kanshi/"$(hostname)"
